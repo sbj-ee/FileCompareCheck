bbbb0e82b5fb539a49a1a1b50b464eee2d30f69e
Stephen B. Johnson <49662809+sbj-ee@users.noreply.github.com>
2025-02-14 07:40:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import hashlib
 import glob
+from collections import Counter
 
+
+def most_frequent(input_list):
+    occurrence_count = Counter(input_list)
+    return occurrence_count.most_common(1)[0][0]
 
 def md5(filename):
     """Compute the md5 for a file."""
@@ -14,9 +19,18 @@ def main():
     test_files = glob.glob('*.txt')
     test_files.sort()
 
+    file_hash_list = []
+    file_hash_dict = {}
     for test_file in test_files:
-        print(f"{md5(test_file)} : {test_file}")
+        file_hash = md5(test_file)
+        print(f"{file_hash} : {test_file}")
+        file_hash_list.append(file_hash)
+        file_hash_dict[file_hash] = test_file
 
+    file_hash_set = set(file_hash_list)
+    print(f"There are {len(file_hash_set)} unique MD5 values")
+    print(f"The most frequent hash is {most_frequent(file_hash_list)}")
+    
 
 if __name__ == "__main__":
     main()
